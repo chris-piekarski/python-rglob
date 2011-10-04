6b611714a80a88681cb3439a822527b6d290bdd7
Christopher Piekarski <christopherpiekarski@yahoo.com>
2011-10-03 22:25:49 -0600

    first commit of rglob

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rglob',
+      version='1.0',
+      description='Python Recursive Glob',
+      author='Christopher Piekarski',
+      author_email='polo1065@gmail.com',
+      keywords=['glob','recursive'],
+      classifiers=[
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Programming Language :: Python :: 2.7',
+                   'Operating System :: OS Independent',
+                   ],
+      url='http://cpiekarski.com/2011/09/23/python-recursive-glob/',
+      py_modules=['__init__.py'],
+     )
