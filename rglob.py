9563c36972af84c8c969ea844dd29b7f2fcc7fb1
Christopher Piekarski <christopherpiekarski@yahoo.com>
2011-10-13 19:14:02 -0600

    added lcount method for counting the number of lines in a set of files

--- rglob.py
+++ rglob.py
@@ -1,18 +1,25 @@
+"""
+Recursive Glob Module
+Methods:
+	rglob(base, pattern)
+	rglob_(pattern)
+	lcount(base, pattern, func=lambda x : True)
+"""
 import glob
 import os
 
 def _getDirs(base):
 	return [x for x in glob.iglob(os.path.join( base, '*')) if os.path.isdir(x) ]
 
-def _count(files):
+def _count(files, func):
 	lines = 0
 	for f in files:
-		lines += sum(1 for l in open(f))
+		lines += sum([1 for l in open(f) if func(l)])
 	return lines
 
 
 def rglob(base, pattern):
-	""" recursive glob starting in specified directory """
+	""" Recursive glob starting in specified directory """
 	flist = []
 	flist.extend(glob.glob(os.path.join(base,pattern)))
 	dirs = _getDirs(base)
@@ -22,16 +29,29 @@ def rglob(base, pattern):
 	return flist
 
 def rglob_(pattern):
-	""" performs a recursive glob in the current working directory """
+	""" Performs a recursive glob in the current working directory """
 	return rglob(os.getcwd(), pattern)
 
-def lcount(base, pattern):
+def lcount(base, pattern, func = lambda x : True):
+	""" Counts the number of lines in each file found matching pattern.
+		Params:
+			base - root directory to start the search
+			pattern - pattern for glob to match (i.e '*.py')
+			func - boolean filter function
+				example: lambda x : True if len(x.strip()) else False #don't count empty lines
+				default: lambda x : True
+	"""
 	allFiles = rglob(base, pattern)
-	return _count(allFiles)
+	return _count(allFiles, func)
 
 if __name__ == "__main__":
+	#filter out empty lines and comments
+	filterFunc = lambda x : True if (len(x.strip()) and x.strip()[0] != '#') else False
+    
 	pyFiles = rglob(os.path.dirname(__file__), "*.py")
-	print pyFiles, " {} total lines".format(_count(pyFiles))
+	print " {} total lines".format(_count(pyFiles, filterFunc))
 	
 	pyFiles_ = rglob_("*.py")
-	print pyFiles_, " {} total lines".format(_count(pyFiles))
+	print " {} total lines".format(_count(pyFiles_, filterFunc))
+	print " {} total lines".format(lcount(os.path.dirname(__file__), "*.py", filterFunc))
+	
